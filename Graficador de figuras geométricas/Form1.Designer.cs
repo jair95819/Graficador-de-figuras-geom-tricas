2a4d5c5c1c693a750d4703e224f7718bd937ae62
Jair <jairestebanca@gmail.com>
2023-11-05 11:00:41 -0500

    Add project files.

---
+++ Graficador de figuras geométricas/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Graficador_de_figuras_geométricas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.txtbx_base_radio_ladoX = new System.Windows.Forms.TextBox();
+            this.lbl_base_radio_lado = new System.Windows.Forms.Label();
+            this.txtbx_alturaY = new System.Windows.Forms.TextBox();
+            this.lbl_altura = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 82);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Circulo";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.opcionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(292, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 200);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Cuadrado";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 128);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Rectángulo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 151);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(69, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Triángulo";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // txtbx_base_radio_ladoX
+            // 
+            this.txtbx_base_radio_ladoX.Location = new System.Drawing.Point(141, 78);
+            this.txtbx_base_radio_ladoX.Name = "txtbx_base_radio_ladoX";
+            this.txtbx_base_radio_ladoX.Size = new System.Drawing.Size(100, 20);
+            this.txtbx_base_radio_ladoX.TabIndex = 6;
+            // 
+            // lbl_base_radio_lado
+            // 
+            this.lbl_base_radio_lado.AutoSize = true;
+            this.lbl_base_radio_lado.Location = new System.Drawing.Point(118, 62);
+            this.lbl_base_radio_lado.Name = "lbl_base_radio_lado";
+            this.lbl_base_radio_lado.Size = new System.Drawing.Size(104, 13);
+            this.lbl_base_radio_lado.TabIndex = 7;
+            this.lbl_base_radio_lado.Text = "Base - Radio - Lado:";
+            // 
+            // txtbx_alturaY
+            // 
+            this.txtbx_alturaY.Location = new System.Drawing.Point(141, 127);
+            this.txtbx_alturaY.Name = "txtbx_alturaY";
+            this.txtbx_alturaY.Size = new System.Drawing.Size(100, 20);
+            this.txtbx_alturaY.TabIndex = 8;
+            // 
+            // lbl_altura
+            // 
+            this.lbl_altura.AutoSize = true;
+            this.lbl_altura.Location = new System.Drawing.Point(118, 111);
+            this.lbl_altura.Name = "lbl_altura";
+            this.lbl_altura.Size = new System.Drawing.Size(37, 13);
+            this.lbl_altura.TabIndex = 9;
+            this.lbl_altura.Text = "Altura:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 24);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Graficar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_altura);
+            this.Controls.Add(this.txtbx_alturaY);
+            this.Controls.Add(this.lbl_base_radio_lado);
+            this.Controls.Add(this.txtbx_base_radio_ladoX);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox txtbx_base_radio_ladoX;
+        private System.Windows.Forms.Label lbl_base_radio_lado;
+        private System.Windows.Forms.TextBox txtbx_alturaY;
+        private System.Windows.Forms.Label lbl_altura;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
