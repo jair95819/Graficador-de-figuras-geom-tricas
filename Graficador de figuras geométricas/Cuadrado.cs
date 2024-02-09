a99b7fc8e67a705e2f573e49b9160c60946f5acd
Jair <jairestebanca@gmail.com>
2024-02-09 14:20:29 -0500

    Cuadrado's Class deleted

--- Graficador de figuras geométricas/Cuadrado.cs
+++
@@ -1,17 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Graficador_de_figuras_geométricas
-{
-    internal class Cuadrado
-    {
-        public int lado;
-
-        public Cuadrado(int lado)        {
-            this.lado = lado;
-        }
-    }
-}
